5e07b4cc97f7bf36c15fbb3b4fae5677b8f1d7ea
Julien Poulton <julien.poulton@gmail.com>
2017-07-06 16:02:25 +0200

    implemented boid slider and repaired a boid bug

--- project/controls.js
+++ project/controls.js
@@ -12,11 +12,9 @@ var sliders = [{
     key: 'coheseWeight',
     label: 'cohesion',
     max: 10
-}, {
-    key: 'friction',
-    max: 0.5
 }];
 
+// Weights
 sliders.forEach(function (opt) {
     var label = opt.label || opt.key;
     var min = opt.min || 0;
@@ -33,3 +31,26 @@ sliders.forEach(function (opt) {
     });
 });
 
+// Individuals
+var html = '<div style="display:flex;">' +
+    '<input id="boids-slider" type="range" min="2" max="198" step="1" value="100"> ' + 
+    'boids' +
+    '</div>';
+controls.append(html);
+
+$('#boids-slider').bind('input', function() {
+    var desired = this.value;
+    var got = boids.length;
+    
+    if (got > desired) {
+        var unwanted = boids.splice(desired);
+        unwanted.forEach(function(b){
+            boidLayer.removeChild(b.graphics);
+        });
+    } else if (got < desired) {
+        for (var i = 0; i < desired - got; ++i) {
+            var b = new Boid(0,0);
+            boids.push(new Boid(Math.random() * app.screen.width, Math.random() * app.screen.height));
+        }
+    }
+});
