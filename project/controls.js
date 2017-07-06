2f48d27afdcad72e6252e8ecce502577a81c4a8e
Julien Poulton <julien.poulton@gmail.com>
2017-07-06 12:10:19 +0200

    included jquery

--- project/controls.js
+++ project/controls.js
@@ -1,4 +1,4 @@
-var controls = document.getElementById("controls")
+var controls = $("#controls")
 // <input id="separation-slider" type="range" min="0" max="10" step="0.1">
-controls.appendChild('<input id="separation-slider" type="range" min="0" max="10" step="0.1">')
+controls.append('<input id="separation-slider" type="range" min="0" max="10" step="0.1">')
 
